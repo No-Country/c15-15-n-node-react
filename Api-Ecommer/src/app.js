507bd3b9d9594f4045350022720d5df7ca25307c
samuelbernal44 <samuelbernal44@gmail.com>
2023-11-25 20:33:03 -0500

    Levantar servidor con express

---
+++ Api-Ecommer/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const server = express();
+
+server.name = 'API';
+
+// Error catching endware.
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
