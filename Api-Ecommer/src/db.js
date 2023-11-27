01c6489ea302dd23f4ad14d34e01c9b80ddc563d
samuelbernal44 <samuelbernal44@gmail.com>
2023-11-26 19:11:49 -0500

    Crear modelos y relaciones en la bbdd

--- Api-Ecommer/src/db.js
+++ Api-Ecommer/src/db.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { Host, Port, User, Password, Databasename } = process.env;
+const fs = require('fs');
+const path = require('path');
+
+const { Host, Port, Userdb, Password, Databasename } = process.env;
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: Host,
   port: Port,
-  username: User,
+  username: Userdb,
   password: Password,
   database: Databasename,
   ssl: true,
@@ -27,7 +30,46 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter(
+    (file) =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
+
+// ACÁ ABAJO IMPORTAR LOS MODELS
+const { User, Product, Order, Detailorder } = sequelize.models;
+
+// Aca van las relaciones
+
+User.hasMany(Order);
+Order.belongsTo(User);
+Order.hasMany(Detailorder);
+Detailorder.belongsTo(Order);
+Detailorder.hasOne(Product);
+Product.belongsTo(Detailorder);
+
 module.exports = {
-  // ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importar la conexión { conn } = require('./db.js');
 };
