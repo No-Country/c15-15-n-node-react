0c3849e6bad8e5c8c0ef5cb834488a1a984f57ea
samuelbernal44 <samuelbernal44@gmail.com>
2023-12-07 18:27:19 -0500

    Crear Producto admite category/subcategory

--- Api-Ecommer/src/handlers/Product/createProductHandler.js
+++ Api-Ecommer/src/handlers/Product/createProductHandler.js
@@ -1,7 +1,14 @@
 const { Product } = require('../../db');
 
 const createProductHandler = async (data) => {
-  const product = await Product.create(data);
+  const product = await Product.create({
+    name: data.name,
+    description: data.description,
+    price: data.price,
+    stock: data.stock,
+    category: data.category,
+    subcategory: data.subcategory,
+  });
   return product;
 };
 
